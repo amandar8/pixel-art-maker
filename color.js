e63647d0ab68a2a43437e114080813e78ee00690
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-05-21 18:59:37 -0500

    clickable colored sqaures

--- color.js
+++ color.js
@@ -1,22 +1,43 @@
 let canvas = document.getElementById('canvas');
-let redColor = document.getElementById('red');
-let blueColor = document.getElementById('blue');
+let fillColor = document.getElementById('colors_container');
 let currentColor = 'red';
+let paletteColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'gray', 'black'];
+// let redColor = document.getElementById('red');
+// let blueColor = document.getElementById('blue');
+// let yellowColor = document.getElementById('yellow');
 
-canvas.addEventListener('click', function (event) {
-    event.target.style.backgroundColor = currentColor;
+canvas.addEventListener('click', function(event) {
+  event.target.style.backgroundColor = currentColor;
 });
 
-redColor.addEventListener('click', function (event) {
-  currentColor = 'red';
-})
+function generateRow() {
+  for (let i = 0; i < 70; i++) {
+    let canvasBox = document.createElement('div');
+    canvasBox.classList.add('canvas-box');
+    canvas.append(canvasBox);
+  }};
+  for (let i = 0; i < 10; i++) {
+    generateRow(i);
+  }
 
-blueColor.addEventListener('click', function (event) {
-  currentColor = 'blue';
-})
-
-for (let i = 0; i<4; i++) {
-  let canvasBox = document.createElement('div');
-  canvasBox.classList.add('canvas-box');
-  canvas.append(canvasBox);
+for (let i = 0; i < paletteColors.length; i++) {
+  let palette = document.createElement('div');
+  palette.classList.add('palette');
+  palette.style.backgroundColor = paletteColors[i];
+  colors_container.append(palette);
+  // document.querySelector('div[id="colors-container"]').appendChild(palette);
 }
+
+fillColor.addEventListener('click', function(event) {
+	currentColor = event.target.style.backgroundColor;
+});
+// for (let i = 0; i < 1; i++) {
+//   let palette = document.createElement('div');
+//   palette.classList.add('blue');
+//   document.querySelector('div[id="colors-container"]').appendChild(palette);
+// }
+//
+// document.querySelector('div[class = "blue"]').addEventListener('click', function(event) {
+//   currentColor = 'blue';
+//   console.log(currentColor);
+// })
