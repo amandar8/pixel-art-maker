4bd6382fb0e749c477e512e3be45b325824f8b0f
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-05-22 09:51:55 -0500

    completed without bonus

--- color.js
+++ color.js
@@ -2,16 +2,13 @@ let canvas = document.getElementById('canvas');
 let fillColor = document.getElementById('colors_container');
 let currentColor = 'red';
 let paletteColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'gray', 'black'];
-// let redColor = document.getElementById('red');
-// let blueColor = document.getElementById('blue');
-// let yellowColor = document.getElementById('yellow');
 
 canvas.addEventListener('click', function(event) {
   event.target.style.backgroundColor = currentColor;
 });
 
 function generateRow() {
-  for (let i = 0; i < 70; i++) {
+  for (let i = 0; i < 100; i++) {
     let canvasBox = document.createElement('div');
     canvasBox.classList.add('canvas-box');
     canvas.append(canvasBox);
@@ -31,13 +28,7 @@ for (let i = 0; i < paletteColors.length; i++) {
 fillColor.addEventListener('click', function(event) {
 	currentColor = event.target.style.backgroundColor;
 });
-// for (let i = 0; i < 1; i++) {
-//   let palette = document.createElement('div');
-//   palette.classList.add('blue');
-//   document.querySelector('div[id="colors-container"]').appendChild(palette);
-// }
-//
-// document.querySelector('div[class = "blue"]').addEventListener('click', function(event) {
-//   currentColor = 'blue';
-//   console.log(currentColor);
-// })
+
+canvas.addEventListener('mouseDown', function(event){
+  event.target.style.backgroundColor = currentColor
+});
