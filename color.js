21d0e06ef48926fc74a44e69158abcdf88559d6f
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-05-22 12:04:06 -0500

    without bonus

--- color.js
+++ color.js
@@ -1,34 +1,45 @@
-let canvas = document.getElementById('canvas');
-let fillColor = document.getElementById('colors_container');
-let currentColor = 'red';
-let paletteColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'gray', 'black'];
+document.addEventListener('DOMContentLoaded', function() {
 
-canvas.addEventListener('click', function(event) {
-  event.target.style.backgroundColor = currentColor;
-});
+  let canvas = document.getElementById('canvas');
+  let fillColor = document.getElementById('colors_container');
+  let currentColor = 'red';
+  let paletteColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'gray', 'black', 'white'];
+  let brush_color = document.getElementById('brush_color');
 
-function generateRow() {
-  for (let i = 0; i < 100; i++) {
-    let canvasBox = document.createElement('div');
-    canvasBox.classList.add('canvas-box');
-    canvas.append(canvasBox);
-  }};
+  // this creates the div squares in the canvas
+  function generateRow() {
+    for (let i = 0; i < 100; i++) {
+      let canvasBox = document.createElement('div');
+      canvasBox.classList.add('canvas-box');
+      canvas.append(canvasBox);
+    }
+  };
   for (let i = 0; i < 10; i++) {
     generateRow(i);
   }
 
-for (let i = 0; i < paletteColors.length; i++) {
-  let palette = document.createElement('div');
-  palette.classList.add('palette');
-  palette.style.backgroundColor = paletteColors[i];
-  colors_container.append(palette);
-  // document.querySelector('div[id="colors-container"]').appendChild(palette);
-}
+  // this creates the palette color divs
+  for (let i = 0; i < paletteColors.length; i++) {
+    let palette = document.createElement('div');
+    palette.classList.add('palette');
+    palette.style.backgroundColor = paletteColors[i];
+    colors_container.append(palette);
+  }
 
-fillColor.addEventListener('click', function(event) {
-	currentColor = event.target.style.backgroundColor;
-});
+  // this allows user to click on palette color and pick up the color
+  fillColor.addEventListener('click', function(event) {
+    currentColor = event.target.style.backgroundColor;
+  });
 
-canvas.addEventListener('mouseDown', function(event){
-  event.target.style.backgroundColor = currentColor
+  // this allows user to add a color to the squares
+  canvas.addEventListener('click', function(event) {
+    event.target.style.backgroundColor = currentColor;
+  });
+
+  // this shows the selected color in the current color palette circle
+  let currentPaletteColor = document.getElementById('colors_container').addEventListener('click', function(event) {
+    brush_color.style.backgroundColor = event.target.style.backgroundColor;
+  });
+
+
 });
